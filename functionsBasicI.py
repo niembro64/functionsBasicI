67f5e20c76c113c80d77a92ab91a7c474604116a
=Eric Niemeyer <=niemeyer.eric@gmail.com>
2022-02-07 22:41:14 -0500

    Oops Here's the right place

--- functionsBasicI.py
+++ functionsBasicI.py
@@ -3,3 +3,32 @@
 def number_of_food_groups():
     return 5
 print(number_of_food_groups())
+
+#2 prediction : 5
+# def number_of_military_branches():
+#     return 5
+# print(number_of_days_in_a_week_silicon_or_triangle_sides() + number_of_military_branches())
+
+def concatenate(b,c):
+    return str(b)+str(c)
+print(concatenate(2,5))
+
+def number_of_oceans_or_fingers_or_continents():
+    b = 100
+    print(b)
+    if b < 10:
+        return 5
+    else:
+        return 10
+    return 7
+print(number_of_oceans_or_fingers_or_continents())
+
+def number_of_days_in_a_week_silicon_or_triangle_sides(b,c):
+    if b<c:
+        return 7
+    else:
+        return 14
+    return 3
+print(number_of_days_in_a_week_silicon_or_triangle_sides(2,3))
+print(number_of_days_in_a_week_silicon_or_triangle_sides(5,3))
+print(number_of_days_in_a_week_silicon_or_triangle_sides(2,3) + number_of_days_in_a_week_silicon_or_triangle_sides(5,3))
